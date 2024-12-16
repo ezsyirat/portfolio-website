ad46c79eaabdab1b801e885bb89319d9822d7883
Muhammad Sirat <ezsyirat@gmail.com>
2024-12-16 20:51:26 +0800

    pass: header dan navbar Finish

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
+// Navbar fixed
+$(window).on("scroll", function () {
+  const $header = $("header");
+  const fixedNav = $header.offset().top;
+
+  if ($(window).scrollTop() > fixedNav) {
+    $header.addClass("navbar-fixed");
+  } else {
+    $header.removeClass("navbar-fixed");
+  }
+});
+
+// Hamburger
+$("#hamburger").on("click", function () {
+  $(this).toggleClass("hamburger-active");
+  $("#nav-menu").toggleClass("hidden");
+});
+
+// Hover running text
 $(document).ready(function () {
     $('#marquee').hover(
       function () {
-        $(this).css('animation-play-state', 'paused'); // Pause animasi saat di-hover
+        $(this).css('animation-play-state', 'paused');
       },
       function () {
-        $(this).css('animation-play-state', 'running'); // Lanjutkan animasi saat hover dihapus
+        $(this).css('animation-play-state', 'running');
       }
     );
   });
